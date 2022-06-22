8e906cd91df30baaab99e2de88b4677770f79911
Spamchat <106868656+Spamchat1@users.noreply.github.com>
2022-06-22 19:58:59 +0200

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -4,15 +4,13 @@ deta = Deta("a0nx7pgk_CAsXSD5UjJsWT8xj9nPSAb14xduJ1fUR")
 chats = deta.Base("spamchat")
 res = chats.fetch()
 all_items = res.items
-#print(all_items)
-#print("-------------")
 all_items.sort(key=lambda x: x["spamscore"])
 for item in all_items:
     h = "The User \"" + item["key"] + "\" has the highest Spamscore with " + item["spamscore"] + "."
 print(h)
 datei = open("old/Readme.md", "r")
-in = datei.read()
+inn = datei.read()
 datei.close()
-datei = open("Readme.md", "a")
-in = datei.write(h)
+datei = open("Readme.md", "w")
+datei.write(inn + h)
 datei.close()
